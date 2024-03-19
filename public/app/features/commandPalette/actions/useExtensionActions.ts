8cc10805a399dfdbbe6a4fa2ead1653414b4b4bf
Marcus Andersson <marcus.andersson@grafana.com>
2024-03-19 14:56:18 +0100

    using the hook instead of the getter when fetching extensions.

--- public/app/features/commandPalette/actions/useExtensionActions.ts
+++ public/app/features/commandPalette/actions/useExtensionActions.ts
@@ -1,16 +1,17 @@
 import { PluginExtensionCommandPaletteContext, PluginExtensionPoints } from '@grafana/data';
-import { getPluginLinkExtensions } from '@grafana/runtime';
+import { usePluginLinkExtensions } from '@grafana/runtime';
 
 import { CommandPaletteAction } from '../types';
 import { EXTENSIONS_PRIORITY } from '../values';
 
-export default function getExtensionActions(): CommandPaletteAction[] {
+export default function useExtensionActions(): CommandPaletteAction[] {
   const context: PluginExtensionCommandPaletteContext = {};
-  const { extensions } = getPluginLinkExtensions({
+  const { extensions } = usePluginLinkExtensions({
     extensionPointId: PluginExtensionPoints.CommandPalette,
     context,
     limitPerPlugin: 3,
   });
+
   return extensions.map((extension) => ({
     section: extension.category ?? 'Extensions',
     priority: EXTENSIONS_PRIORITY,
